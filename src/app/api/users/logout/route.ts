3eca6574f1f67ee88e2a8ae2ad6f6826b63de688
Aniruddha <aniruddhathakre7@gmail.com>
2025-05-23 23:43:14 +0530

    logout route catch block updated

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -9,7 +9,13 @@ export async function GET() {
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
 
     return response;
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
